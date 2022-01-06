343cfa6185eaa6592104caf9ac39ae59886028cd
Jeison Osorio Alarcon <jalarcon@azteca-comunicaciones.com>
2022-01-06 16:03:28 -0500

    Publish done - Latest changes

---
+++ db/datastatic.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 04-01-2022 a las 21:43:26
+-- Versión del servidor: 10.5.12-MariaDB-cll-lve
+-- Versión de PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `u644237725_mtcalc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datastatic`
+--
+
+CREATE TABLE `datastatic` (
+  `id` int(11) NOT NULL,
+  `concept` varchar(100) NOT NULL,
+  `url_imagen` varchar(600) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `datastatic`
+--
+
+INSERT INTO `datastatic` (`id`, `concept`, `url_imagen`) VALUES
+(1, 'Importación normal - Marca DT Spare Parts', 'https://www.dt-spareparts.com/fileadmin/_processed_/a/e/csm_Benefits_07_dc234fe755.jpg'),
+(2, 'Importación normal - Marca Sampa', 'https://femaas.com/media/1082/febs-_0008_sampa.jpg'),
+(3, 'Importación exprés - Marca DT Spare Parts', 'https://www.dt-spareparts.com/fileadmin/_processed_/a/e/csm_Benefits_07_dc234fe755.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `datastatic`
+--
+ALTER TABLE `datastatic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `datastatic`
+--
+ALTER TABLE `datastatic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
